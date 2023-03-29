d70b0ecb3a83fef107ff5b4e9df260ae0c5516ab
guguping <qkfhandur@naver.com>
2023-03-29 14:44:28 +0900

    study2

--- day2/MySQL_dbStudy.sql
+++ day2/MySQL_dbStudy.sql
@@ -310,15 +310,21 @@ select * from emp where sal=800;
 -- 서브 쿼리 적용
 select * from emp where sal=(select min(sal) from emp);
 
-
 -- 최고 급여를 받는 사원 정보 조회
 select * from emp where sal = (select max(sal) from emp);
 -- allen 보다 높은 급여를 받는 사원 조회
 select * from emp where sal > (select sal from emp where ename = 'allen');
 
 
-
-
+-- 연습문제 
+-- 1. clark 보다 늦게 입사한 사원 조회 
+select * from emp where hiredate > (select hiredate from emp where ename = 'clark');
+-- 2. 부서번호가 20인 사원 중에서 전체 사원 평균 급여보다 높은 급여를 받는 사원 조회 
+select * from emp where deptno = 20 and sal > (select avg(sal) from emp);
+-- 3. 2번 조회 결과에서 부서이름, 부서위치도 함께 조회 
+select * from emp e , dept d 
+			where e.deptno = 20 and d.deptno = 20 
+				and sal > (select avg(sal) from emp);
 
 
 
